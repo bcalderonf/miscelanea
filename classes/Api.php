29c83ae23931bfe5ba76d2e5363eff1927dbc6b7
Roger Sosa <RSpro@Rogers-MacBook-Pro.local>
2019-06-14 14:00:14 -0600

    #4 Mail Cierre

--- classes/Api.php
+++ classes/Api.php
@@ -6,6 +6,12 @@ include("Controller.class.php");
 include("Security.class.php");
 include("permissions.inc");
 
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class Api extends Controller  {
 
@@ -1719,7 +1725,39 @@ public function hacerDevolucion($table, $data, $data_detalle) {
       
         if($queryCierre) {
 
-            
+            $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+            $mail->isSMTP();                                            // Set mailer to use SMTP
+            $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'rogerthedeveloper@gmail.com';                     // SMTP username
+            $mail->Password   = '';                               // SMTP password
+            $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+            $mail->Port       = 587;                                    // TCP port to connect to
+        
+            //Recipients
+            $mail->setFrom('rogerthedeveloper@gmail.com', 'SISTEMA');
+            $mail->addAddress('rogerthedeveloper@gmail.com', 'Roger Sosa');     // Add a recipient
+            $mail->addAddress('tonyvaliente@gmail.com', 'Tony Valiente');     // Add a recipient
+
+            // Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+            $mail->send();
+        } catch (Exception $e) {
+
+        }
+       
 
         }
 
@@ -1730,7 +1768,7 @@ public function hacerDevolucion($table, $data, $data_detalle) {
 
 if(isset($_POST["data"]) && isset($_GET["action"])) {
 
-       
+
         $data = $_POST["data"];
 
         if(isset($_POST["table"])) {
